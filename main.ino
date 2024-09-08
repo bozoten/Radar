4a720273a3f4f2cb359b3d83a5cfea0b0a9b5817
bozoten <shridharapavel@gmail.com>
2024-09-08 19:56:13 -0400

    Made the servo stop & the light stay only when an object is detected (It works)

--- main.ino
+++ main.ino
@@ -1,12 +1,12 @@
 #include <Servo.h>
 
-int lightPin = 13;
-int servoPin = 10;
+int lightPin = 12;
+int servoPin = 11;
 int servoAngle = 180;
 int delayAmount = 1000;
 int br = 115200;
-int trigPin = 11;
-int echoPin = 9;
+int trigPin = 9;
+int echoPin =  10;
 
 Servo angler;
 
@@ -31,7 +31,7 @@ void loop() {
   // Read the echo
   duration = pulseIn(echoPin, HIGH);
   
-  // Calculate distance in centimeters
+  // Calculate distance in centimeters using the speed of sound
   distance = duration * 0.034 / 2;
   
   // Print distance to Serial Monitor
@@ -39,15 +39,17 @@ void loop() {
   Serial.print(distance);
   Serial.println(" cm");
   
-  // Check if object is close enough (you can adjust this threshold)
+  // Check if object is close enough
   if (distance < 20) {  // Object is within 20 cm
     digitalWrite(lightPin, HIGH);  // Turn on the LED
   } else {
-    digitalWrite(lightPin, LOW);   // Turn off the LED
+    digitalWrite(lightPin, HIGH);  // Turn on the LED
+    delay(200);
+    digitalWrite(lightPin, LOW); 
+    delay(200);  // Turn off the LED
+    angler.write(servoAngle);
+    delay(delayAmount);
+    angler.write(0);
+    delay(delayAmount);
   }
-  
-  // Move the servo (if needed)
-  angler.write(servoAngle);
-  
-
 }
