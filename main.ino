e2f383138b932fee89ac4375e239422e55d7fbb4
bozoten <shridharapavel@gmail.com>
2024-09-08 18:44:53 -0400

    Made Ultrasonic sensor detect objects closer than 20 cm

--- main.ino
+++ main.ino
@@ -1,22 +1,53 @@
-
 #include <Servo.h>
 
+int lightPin = 13;
 int servoPin = 10;
 int servoAngle = 180;
-int delayAmout = 1000;
+int delayAmount = 1000;
+int br = 115200;
+int trigPin = 11;
+int echoPin = 9;
 
 Servo angler;
 
 void setup() {
-  // Attach the servo's pin number to the servo object
+  pinMode(lightPin, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   angler.attach(servoPin);
+  Serial.begin(br);
 }
 
 void loop() {
-  // Set the servo angle
-  angler.write(servoAngle);
-  delay(delayAmout);
-  angler.write(0);
-  delay(delayAmout);
+  long duration, distance;
   
+  // Trigger the ultrasonic sensor
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Read the echo
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate distance in centimeters
+  distance = duration * 0.034 / 2;
+  
+  // Print distance to Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+  // Check if object is close enough (you can adjust this threshold)
+  if (distance < 20) {  // Object is within 20 cm
+    digitalWrite(lightPin, HIGH);  // Turn on the LED
+  } else {
+    digitalWrite(lightPin, LOW);   // Turn off the LED
+  }
+  
+  // Move the servo (if needed)
+  angler.write(servoAngle);
+  
+
 }
